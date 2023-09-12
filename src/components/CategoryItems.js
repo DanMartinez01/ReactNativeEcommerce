2c5f3afa7930b6503aa3d727b5de14515e4cd410
Daniela <martinezdl_@hotmail.com>
2023-09-12 20:36:08 -0300

    create CategoryItems component

---
+++ src/components/CategoryItems.js
@@ -0,0 +1,31 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+const CategoryItem = ({ item }) => {
+  return (
+    <View>
+      <Text style={styles.categoryText}>{item}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  categoryText: {
+    marginHorizontal: 20,
+    marginVertical: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    fontFamily: "Roboto-Regular",
+
+    // BORDER STYLES
+    borderColor: "black",
+    borderWidth: 2,
+    borderRadius: 20,
+
+    // Text styles
+    textAlign: "center",
+    padding: 8,
+  },
+});
+
+export default CategoryItem;
