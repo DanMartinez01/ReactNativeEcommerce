b1e70d6171f05f01167c079ee1e10c2535712b56
Daniela <martinezdl_@hotmail.com>
2023-11-08 22:45:35 -0300

    create home screen

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,41 +1,73 @@
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 import React from "react";
+import { useTheme } from "react-native-paper";
 import Header from "../src/components/Header";
-import Categories from "../src/components/Categories";
-import Products from "./Products";
-import ProductItem from "../src/components/ProductItem";
+// import Categories from "../src/components/Categories";
+// import Products from "./Products";
+// import ProductItem from "../src/components/ProductItem";
 import CategoryItem from "../src/components/CategoryItem";
 // import { categories } from "../Data/categories";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useSelector } from "react-redux";
+import { useGetCategoriesQuery } from "../src/services/ecApi";
+import { ScrollView } from "react-native-web";
 
 const Home = ({ navigation }) => {
-  const categories = useSelector((state) => state.homeSlice.allCategories);
-  console.log(categories);
-  return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <Header title="Categorías" />
-        {/* <Products navigation={navigation} /> */}
+  const theme = useTheme();
+  const {
+    data: categories,
+    isLoading,
+    isError,
+    error,
+  } = useGetCategoriesQuery();
 
-        <FlatList
-          data={categories}
-          keyExtractor={(key) => key}
-          renderItem={({ item }) => (
-            <CategoryItem navigation={navigation} item={item} />
-          )}
-        />
-      </View>
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      {isLoading ? (
+        <View>
+          <ActivityIndicator style={{}} size="small" color="#0000ff" />
+        </View>
+      ) : (
+        <>
+          <ScrollView
+            style={{
+              backgroundColor: "black",
+              dark: true,
+            }}
+          >
+            <Header title="Bibliografia" style={{ color: "white" }} />
+            <FlatList
+              style={styles.flat}
+              data={categories}
+              keyExtractor={(key) => key}
+              numColumns={2}
+              onEndReached={() => console.log("Aquí")}
+              renderItem={({ item }) => (
+                <CategoryItem navigation={navigation} item={item} />
+              )}
+            />
+          </ScrollView>
+        </>
+      )}
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 20,
+  },
+  flat: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    paddingHorizontal: "auto",
+    margin: "auto",
   },
 });
 
