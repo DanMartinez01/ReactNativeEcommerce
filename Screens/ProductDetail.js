77e86f57e72a089aacc3932fbb33303c2496985a
Daniela <martinezdl_@hotmail.com>
2023-11-08 22:46:09 -0300

    create product details screen

--- Screens/ProductDetail.js
+++ Screens/ProductDetail.js
@@ -1,52 +1,74 @@
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  Pressable,
+} from "react-native";
 import React from "react";
-import { View, Text, Image, StyleSheet, Pressable } from "react-native";
-import { products } from "../Data/products";
+import Header from "../src/components/Header";
+// import { AntDesign } from "@expo/vector-icons";
+import { colors } from "../src/theme/colors";
+import { useSelector } from "react-redux";
 
-function ProductDetail({ route }) {
-  const selectedItem = route.params.selectedItem;
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}> {selectedItem.title} </Text>
-      <Image
-        style={styles.image}
-        source={{ uri: selectedItem.images[0] }}
-        resizeMode="cover"
-      />
-      <Text style={styles.text}>Brand: {selectedItem.brand}</Text>
-      <Text style={styles.text}>Description: {selectedItem.description}</Text>
-      <Text style={styles.text}>Price: ${selectedItem.price}</Text>
-      <Text style={styles.text}>Rating: {selectedItem.rating}</Text>
-      <Pressable onPress={() => console.log("added")}>
-        <Text>Add to cart</Text>
-      </Pressable>
-    </View>
+const ProductDetail = ({ navigation, route }) => {
+  // const { item } = route.params;
+
+  const productSelected = useSelector(
+    (state) => state.homeSlice.productSelected
   );
-}
+
+  return (
+    <SafeAreaView style={{ backgroundColor: "black", flex: 1 }}>
+      <Header title="Detalle" navigation={navigation} />
+      <View style={styles.containerImage}>
+        <Image
+          style={styles.image}
+          source={{
+            uri: productSelected.images[0],
+          }}
+        />
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Text style={styles.title}> {productSelected.title} </Text>
+        </View>
+        <Text style={styles.description}> {productSelected.description} </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    marginHorizontal: 20,
-    marginVertical: 10,
-    borderColor: "#373d20",
-    borderRadius: 10,
-    borderWidth: 1,
-    justifyContent: "space-between",
-    flexDirection: "column",
-    padding: 20,
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: "900",
-    marginBottom: 20,
-    color: "#373d20",
-    textAlign: "left",
-    fontFamily: "",
-  },
   image: {
-    marginBottom: 10,
-    borderRadius: 50,
-    height: 300,
-    width: 300,
+    height: 350,
+    width: 350,
+  },
+  containerImage: {
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: "Dancing",
+    marginTop: 15,
+    color: "white",
+  },
+  price: {
+    fontSize: 18,
+    fontFamily: "Dancing",
+    color: "white",
+  },
+  description: {
+    fontSize: 18,
+    marginTop: 12,
+    marginBottom: 30,
+    marginHorizontal: 10,
+    color: "white",
   },
 });
+
 export default ProductDetail;
