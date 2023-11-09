5ffd766d19d5d1c60c412248732befcddf423e9c
Daniela <martinezdl_@hotmail.com>
2023-11-08 22:42:17 -0300

    create redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import homeSlice from "./slices/homeSlice";
+import authSlice from "./slices/authSlice";
+import { ecApi } from "../services/ecApi";
 
 export const store = configureStore({
-  reducer: { homeSlice },
+  reducer: {
+    homeSlice,
+    [ecApi.reducerPath]: ecApi.reducer,
+    authSlice,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(ecApi.middleware),
 });
